4b03c28c95dfd563e00c6bb0a600b1f0dfb8c5d9
jtskidmore <johntoddskidmore@gmail.com>
2020-09-02 10:51:11 -0600

    persisted comments and added delete buttons for comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
+  let commentHash = {}
 
   let likeButton = document.getElementsByClassName("like-button")[0];
   let commentForm = document.getElementsByClassName("comment-form")[0];
@@ -31,9 +32,15 @@ document.addEventListener("DOMContentLoaded", function() {
         let listElement = document.createElement("li")
         listElement.textContent = comment.content
         commentsList.append(listElement)
+        let deleteButton = document.createElement("button")
+        deleteButton.textContent = "Remove"
+        commentsList.append(deleteButton)
+
+        commentHash[comment.content] = comment.id
       })
 
     })
+
 
     likeButton.addEventListener("click", function(e) {
       let imageLikes = ++e.target.parentNode.children[0].textContent.split(" ")[0]
@@ -69,9 +76,50 @@ document.addEventListener("DOMContentLoaded", function() {
       let commentListElement = document.createElement("li")
       commentListElement.textContent = comment
       commentsList.append(commentListElement)
+      let deleteButton = document.createElement("button")
+      deleteButton.textContent = "Remove"
+      commentsList.append(deleteButton)
 
       e.target.children[0].value = ""
+//////////////////////////////////////////////////////////////////
+      let data = {
+        imageId: 1,
+        content: comment
+      }
 
+      let configObj = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accepts": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+
+      fetch("http://localhost:3000/comments", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data)
+        })
+//////////////////////////////////////////////////////////////////
+
+    })
+
+    commentsList.addEventListener("click", function(e) {
+      if (e.target.tagName == "BUTTON") {
+        let listElement = e.target.previousSibling
+        let commentId = commentHash[e.target.previousSibling.textContent]
+        console.log(commentId)
+        listElement.remove();
+        e.target.remove();
+
+        fetch(`http://localhost:3000/comments/${commentId}`, {
+          method: "DELETE"
+        })
+
+      }
     })
 
 })
