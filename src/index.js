2d0e95948511b8cf42d37bcc3a5da7cc6a3509f9
jtskidmore <johntoddskidmore@gmail.com>
2020-09-02 10:16:37 -0600

    core deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
 // write your code here
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  let likeButton = document.getElementsByClassName("like-button")[0];
+  let commentForm = document.getElementsByClassName("comment-form")[0];
+  let commentsList = document.getElementsByClassName('comments')[0];
+
+  console.log(commentForm)
+
+  fetch("http://localhost:3000/images/1")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let dogImage = document.getElementsByClassName("image")[0];
+      dogImage.src = data.image
+
+      let dogTitle = document.getElementsByClassName("title")[0];
+      dogTitle.textContent = data.title
+
+      let dogLikes = document.getElementsByClassName("likes")[0];
+      dogLikes.textContent = `${data.likes} likes`;
+
+      commentsList.innerHTML = "";
+
+      let dataComments = data.comments
+      dataComments.forEach(function(comment) {
+        let listElement = document.createElement("li")
+        listElement.textContent = comment.content
+        commentsList.append(listElement)
+      })
+
+    })
+
+    likeButton.addEventListener("click", function(e) {
+      let imageLikes = ++e.target.parentNode.children[0].textContent.split(" ")[0]
+      e.target.parentNode.children[0].textContent = `${imageLikes} likes`
+
+      let data = {
+        likes: imageLikes
+      }
+
+      let configObj = {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json",
+          "Accepts": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+
+      fetch("http://localhost:3000/images/1", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          console.log(data)
+        })
+    })
+
+    commentForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      let comment = e.target.children[0].value
+      console.log(comment)
+
+      let commentListElement = document.createElement("li")
+      commentListElement.textContent = comment
+      commentsList.append(commentListElement)
+
+      e.target.children[0].value = ""
+
+    })
+
+})
