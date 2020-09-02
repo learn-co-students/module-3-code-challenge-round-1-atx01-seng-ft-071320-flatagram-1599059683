48bee2edc0cea25051d1fcaa16f2a9615fe2d6c0
Marc Majcher <majcher@gmail.com>
2020-09-02 15:00:50 -0500

    marc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,77 @@
-// write your code here
+const URL = "http://localhost:3000";
+const imageURL = `${URL}/images/1`;
+const commentsURL = `${URL}/comments`;
+
+let likes = 0;
+let comments = [];
+
+const headers = {
+  Accepts: "application/json",
+  "Content-type": "application/json",
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(imageURL)
+    .then((res) => res.json())
+    .then((image) => renderImage(image));
+
+  fetch(commentsURL)
+    .then((res) => res.json())
+    .then((comments) => populateComments(comments));
+
+  getClass("like-button").addEventListener("click", addLike);
+  getClass("comment-form").addEventListener("submit", submitComment);
+});
+
+function getClass(c) {
+  return document.querySelector(`.${c}`);
+}
+
+function renderImage(img) {
+  likes = img.likes;
+  getClass("title").innerText = img.title;
+  getClass("image").src = img.image;
+  renderLikes();
+}
+
+function renderLikes() {
+  getClass("likes").innerText = `${likes} likes`;
+}
+
+function populateComments(myComments) {
+  comments = myComments;
+  renderComments();
+}
+
+function renderComments() {
+  getClass("comments").innerHTML = comments
+    .map((comment) => `<li>${comment.content}</li>`)
+    .join("");
+}
+
+function addLike() {
+  likes += 1;
+
+  fetch(imageURL, {
+    method: "PATCH",
+    headers,
+    body: JSON.stringify({ likes }),
+  });
+  renderLikes();
+}
+
+function submitComment(e) {
+  e.preventDefault();
+
+  const comment = { content: e.target.comment.value };
+  comments.push(comment);
+
+  fetch(commentsURL, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(comment),
+  });
+
+  e.target.comment.value = "";
+  renderComments();
+}
