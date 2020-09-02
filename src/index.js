64c4fdc39655ab161b2baabc23912f3930a2d402
reedanna <reedanna@mail.com>
2020-09-02 11:26:15 -0500

    extra deliverables finished

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ let likes = "";
 
 document.addEventListener("DOMContentLoaded", (e) => {
     const likeButton = document.querySelector(".like-button");
+    const dislikeButton = document.querySelector(".dislike-button");
     const imgLikes = document.querySelector(".likes");
     const imgComments = document.querySelector(".comments");
     const commentForm = document.querySelector(".comment-form")
@@ -14,10 +15,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
         likeImage(imgLikes);
     });
 
+    dislikeButton.addEventListener("click", (e) => {
+        dislikeImage(imgLikes);
+    });
+
     commentForm.addEventListener("submit", (e) => {
         event.preventDefault();
-        loadComment(commentForm.comment.value, imgComments);
-        commentForm.comment.value = "";
+        addComment(commentForm, imgComments);
     })
 });
 
@@ -31,7 +35,7 @@ function loadImage(imgLikes, imgComments) {
             img.src = data.image;
             imgComments.innerHTML = "";
             data.comments.forEach(comment => {
-                loadComment(comment.content, imgComments);
+                loadComment(comment, imgComments);
             })
             likes = data.likes;
             imgLikes.innerText = `${likes} likes`;
@@ -52,8 +56,51 @@ function likeImage(imgLikes) {
     imgLikes.innerText = `${likes} likes`;
 };
 
+function dislikeImage(imgLikes) {
+    likes--;
+    fetch(`${baseURL}/images/1`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "likes": likes
+        }),
+    })
+    imgLikes.innerText = `${likes} likes`;
+};
+
 function loadComment(comment, imgComments) {
     const commentLi = document.createElement("li");
-    commentLi.innerText = comment;
+    commentLi.innerText = comment.content;
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "delete";
+    commentLi.append(deleteButton);
     imgComments.append(commentLi);
+    deleteButton.addEventListener("click", (e) => {
+        fetch(`${baseURL}/comments/${comment.id}`, {
+            method: 'DELETE'
+        })
+        commentLi.remove();
+    })
+};
+
+function addComment(commentForm, imgComments) {
+    let comment = ""
+    fetch(`${baseURL}/comments`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "imageId": 1,
+            "content": commentForm.comment.value
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            comment = data;
+            loadComment(comment, imgComments);
+            commentForm.comment.value = "";
+        });
 };
